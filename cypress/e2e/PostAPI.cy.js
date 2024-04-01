3f1c2b55a4725e87cbded9297886eaf1d07b7121
mariaa595 <mariakhan5950@gmail.com>
2024-04-01 16:35:57 +0500

    added post tecase

--- cypress/e2e/PostAPI.cy.js
+++ cypress/e2e/PostAPI.cy.js
@@ -9,6 +9,12 @@ describe('POST calls',()=>{
     }
     it(' POST CALL - JSON', () => {
         let emailadress = generateEmail();
+        let payload= {
+            "name": "AB Test 01",
+            "email": emailadress,
+            "gender": "female",
+            "status": "active"
+        }
         cy.log("Email address: " + emailadress)
 
         cy.request({
@@ -18,12 +24,7 @@ describe('POST calls',()=>{
 
                 Authorization: "Bearer d4ad2864a53460445608e94b80d392914d1ccfb3475eb2306ceafb7433f62d17"
             },
-            body: {
-                "name": "AB Test 01",
-                "email": emailadress,
-                "gender": "female",
-                "status": "active"
-            }
+            body: payload
 
         }).then((response) => {
             expect(response.status).to.equal(201)
@@ -34,5 +35,27 @@ describe('POST calls',()=>{
         })
 
 
+    })
+    it(' POST CALL - Fixtures', () => {
+        cy.fixture('users').then((responseObject) => {
+            responseObject.email = generateEmail()
+
+            cy.request({
+                method: 'POST',
+                url: 'https://gorest.co.in/public/v2/users',
+                headers: {
+
+                    Authorization: 'Bearer d4ad2864a53460445608e94b80d392914d1ccfb3475eb2306ceafb7433f62d17'
+                },
+                body: responseObject
+
+            }).then((response) => {
+                expect(response.status).to.equal(201)
+                expect(response.body).has.property("name", "Tester 01")
+                expect(response.body).has.property("gender", "female")
+                expect(response.body).has.property("status", "active")
+                expect(response.body.id).to.not.be.null
+            })
+        })
     })
 })
